6ff9600aa6b7e7db5bff17e9e2eba1d1267ae492
mcvinding <mikkel.vinding@gmail.com>
2021-02-04 14:47:31 +0100

    added example script find_files.m

---
+++ example_scripts/find_files.m
@@ -0,0 +1,47 @@
+function [files] = find_files(folder, inc_str, exc_str)
+% Find all files withing a folder with common filename, e.g.
+% raw data files from the same condition.
+% USE: [files] = find_files(folder, inc_str, exc_str)
+% 
+% folder  : The folder where the files are located
+% inc_str : String that all files have in common (e.g. "taskA")
+% exc_str : String that exclude files.
+
+% Init.
+if ischar(inc_str)
+    inc_str = {inc_str};
+elseif ~iscell(inc_str)
+    warning('Inclusion string must be a string or cell-array with strings.')
+    return
+end
+
+if ~isempty(exc_str)
+    if ischar(exc_str)
+        exc_str = {exc_str};
+    elseif ~iscell(exc_str)
+        warning('Exclusion string must be a string, cell-array with strings, or empty.')
+        return
+    end
+end
+    
+% Find files in folder
+f = dir(folder);
+f = {f.name};
+f = f(~(strcmp('.',f)|strcmp('..',f)));     % Remove dots
+
+% Find match
+files = f;
+for ii = 1:length(inc_str)
+    targetstr = inc_str{ii};
+    files =  files(~cellfun(@isempty, (strfind(files, targetstr))));
+end
+
+for ii = 1:length(exc_str)
+    targetstr = exc_str{ii};
+    files =  files(cellfun(@isempty, (strfind(files, targetstr))));
+end
+
+% Sort for good measure
+sort(files);
+
+%END
